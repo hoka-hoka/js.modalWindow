91e61d6d8ec7347551bb2bd439d05b4da02472ee
hoka-hoka <sff1@tpu.ru>
2021-06-16 15:35:13 +0700

    production

--- build/webpack.paths.config.js
+++ build/webpack.paths.config.js
@@ -1,11 +1,10 @@
-
-const path = require('path'); // утилиты для работы с путями
+const path = require("path"); // утилиты для работы с путями
 const PATHS = {
-  src: path.join(__dirname, '../src'),
-  src_home: path.join(__dirname, '../src/home.ts'),
-  dist: path.join(__dirname, '../dist'),
-  assets: 'assets/'
-}
+  src: path.join(__dirname, "../src"),
+  src_home: path.join(__dirname, "../src/home.ts"),
+  dist: path.join(__dirname, "../docs"),
+  assets: "assets/",
+};
 module.exports = {
-  PATHS: PATHS
-}
+  PATHS: PATHS,
+};
